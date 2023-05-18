32b1c6c5815be96b755bdc40748fe77d48532850
elton <eltonmeurer2022@gmail.com>
2023-05-18 12:12:54 -0300

    Upload

--- networkapi/ambiente/urls_environment.py
+++ networkapi/ambiente/urls_environment.py
@@ -1,8 +1,65 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import
+# # -*- coding: utf-8 -*-
+# from __future__ import absolute_import
 
-from django.conf.urls import patterns
-from django.conf.urls import url
+# from django.conf.urls import patterns
+# from django.conf.urls import url
+
+# from networkapi.ambiente.resource.EnvironmentAllGetByEnvironmentVipResource import EnvironmentAllGetByEnvironmentVipResource
+# from networkapi.ambiente.resource.EnvironmentBlocks import EnvironmentBlocks
+# from networkapi.ambiente.resource.EnvironmentConfigurationAddResource import EnvironmentConfigurationAddResource
+# from networkapi.ambiente.resource.EnvironmentConfigurationListResource import EnvironmentConfigurationListResource
+# from networkapi.ambiente.resource.EnvironmentConfigurationRemoveResource import EnvironmentConfigurationRemoveResource
+# from networkapi.ambiente.resource.EnvironmentEnvironmentVipAssociationResource import EnvironmentEnvironmentVipAssociationResource
+# from networkapi.ambiente.resource.EnvironmentGetAclPathsResource import EnvironmentGetAclPathsResource
+# from networkapi.ambiente.resource.EnvironmentGetByIdResource import EnvironmentGetByIdResource
+# from networkapi.ambiente.resource.EnvironmentListResource import EnvironmentListResource
+# from networkapi.ambiente.resource.EnvironmentSetTemplateResource import EnvironmentSetTemplateResource
+
+# environment_get_by_id_resource = EnvironmentGetByIdResource()
+# environment_get_acl_paths_resource = EnvironmentGetAclPathsResource()
+# environment_set_template = EnvironmentSetTemplateResource()
+# environment_blocks = EnvironmentBlocks()
+# environment_configuration_add_resource = EnvironmentConfigurationAddResource()
+# environment_configuration_list_resource = EnvironmentConfigurationListResource()
+# environment_configuration_remove_resource = EnvironmentConfigurationRemoveResource()
+# environment_environment_vip_association = EnvironmentEnvironmentVipAssociationResource()
+# environment_environment_vip_list = EnvironmentAllGetByEnvironmentVipResource()
+# environment_list_resource = EnvironmentListResource()
+
+# urlpatterns = patterns(
+#     '',
+#     url(r'^id/(?P<environment_id>[^/]+)/$', environment_get_by_id_resource.handle_request,
+#         name='environment.search.by.pk'),
+#     url(r'^acl_path[/]$', environment_get_acl_paths_resource.handle_request,
+#         name='environment.acl_path'),
+#     url(r'^set_template/(?P<environment_id>[^/]+)/$', environment_set_template.handle_request,
+#         name='environment.set.template'),
+#     url(r'^get_env_template/$', environment_set_template.handle_request,
+#         name='environment.get.template'),
+#     url(r'^configuration/save/$', environment_configuration_add_resource.handle_request,
+#         name='environment.configuration.save'),
+#     url(r'^configuration/list/(?P<environment_id>[^/]+)/$', environment_configuration_list_resource.handle_request,
+#         name='environment.configuration.list'),
+#     url(r'^configuration/remove/(?P<environment_id>[^/]+)/(?P<configuration_id>[^/]+)/$', environment_configuration_remove_resource.handle_request,
+#         name='environment.configuration.remove'),
+#     url(r'^save_blocks/$', environment_blocks.handle_request,
+#         name='environment.save.blocks'),
+#     url(r'^update_blocks/$', environment_blocks.handle_request,
+#         name='environment.update.blocks'),
+#     url(r'^get_blocks/(?P<environment_id>[^/]+)/$', environment_blocks.handle_request,
+#         name='environment.get.blocks'),
+#     url(r'^list_no_blocks/$', environment_list_resource.handle_request,
+#         name='environment.list.no_blocks'),
+#     url(r'^(?P<environment_id>[^/]+)/environmentvip/(?P<environment_vip_id>[^/]+)/$', environment_environment_vip_association.handle_request,
+#         name='environment.environmentvip.associate.disassociate'),
+#     url(r'^environmentvip/(?P<environment_vip_id>[^/]+)/$', environment_environment_vip_list.handle_request,
+#         name='environment.environmentvip.list'),
+# )
+# -*- coding: utf-8 -*-
+
+#new 
+
+from django.urls import re_path
 
 from networkapi.ambiente.resource.EnvironmentAllGetByEnvironmentVipResource import EnvironmentAllGetByEnvironmentVipResource
 from networkapi.ambiente.resource.EnvironmentBlocks import EnvironmentBlocks
@@ -26,32 +83,31 @@ environment_environment_vip_association = EnvironmentEnvironmentVipAssociationRe
 environment_environment_vip_list = EnvironmentAllGetByEnvironmentVipResource()
 environment_list_resource = EnvironmentListResource()
 
-urlpatterns = patterns(
-    '',
-    url(r'^id/(?P<environment_id>[^/]+)/$', environment_get_by_id_resource.handle_request,
+urlpatterns = [
+    re_path(r'^id/(?P<environment_id>[^/]+)/$', environment_get_by_id_resource.handle_request,
         name='environment.search.by.pk'),
-    url(r'^acl_path[/]$', environment_get_acl_paths_resource.handle_request,
+    re_path(r'^acl_path[/]$', environment_get_acl_paths_resource.handle_request,
         name='environment.acl_path'),
-    url(r'^set_template/(?P<environment_id>[^/]+)/$', environment_set_template.handle_request,
+    re_path(r'^set_template/(?P<environment_id>[^/]+)/$', environment_set_template.handle_request,
         name='environment.set.template'),
-    url(r'^get_env_template/$', environment_set_template.handle_request,
+    re_path(r'^get_env_template/$', environment_set_template.handle_request,
         name='environment.get.template'),
-    url(r'^configuration/save/$', environment_configuration_add_resource.handle_request,
+    re_path(r'^configuration/save/$', environment_configuration_add_resource.handle_request,
         name='environment.configuration.save'),
-    url(r'^configuration/list/(?P<environment_id>[^/]+)/$', environment_configuration_list_resource.handle_request,
+    re_path(r'^configuration/list/(?P<environment_id>[^/]+)/$', environment_configuration_list_resource.handle_request,
         name='environment.configuration.list'),
-    url(r'^configuration/remove/(?P<environment_id>[^/]+)/(?P<configuration_id>[^/]+)/$', environment_configuration_remove_resource.handle_request,
+    re_path(r'^configuration/remove/(?P<environment_id>[^/]+)/(?P<configuration_id>[^/]+)/$', environment_configuration_remove_resource.handle_request,
         name='environment.configuration.remove'),
-    url(r'^save_blocks/$', environment_blocks.handle_request,
+    re_path(r'^save_blocks/$', environment_blocks.handle_request,
         name='environment.save.blocks'),
-    url(r'^update_blocks/$', environment_blocks.handle_request,
+    re_path(r'^update_blocks/$', environment_blocks.handle_request,
         name='environment.update.blocks'),
-    url(r'^get_blocks/(?P<environment_id>[^/]+)/$', environment_blocks.handle_request,
+    re_path(r'^get_blocks/(?P<environment_id>[^/]+)/$', environment_blocks.handle_request,
         name='environment.get.blocks'),
-    url(r'^list_no_blocks/$', environment_list_resource.handle_request,
+    re_path(r'^list_no_blocks/$', environment_list_resource.handle_request,
         name='environment.list.no_blocks'),
-    url(r'^(?P<environment_id>[^/]+)/environmentvip/(?P<environment_vip_id>[^/]+)/$', environment_environment_vip_association.handle_request,
+    re_path(r'^(?P<environment_id>[^/]+)/environmentvip/(?P<environment_vip_id>[^/]+)/$', environment_environment_vip_association.handle_request,
         name='environment.environmentvip.associate.disassociate'),
-    url(r'^environmentvip/(?P<environment_vip_id>[^/]+)/$', environment_environment_vip_list.handle_request,
+    re_path(r'^environmentvip/(?P<environment_vip_id>[^/]+)/$', environment_environment_vip_list.handle_request,
         name='environment.environmentvip.list'),
-)
+]
--- networkapi/ambiente/urls_logicalenvironment.py
+++ networkapi/ambiente/urls_logicalenvironment.py
@@ -1,8 +1,31 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import
+# # -*- coding: utf-8 -*-
+# from __future__ import absolute_import
 
-from django.conf.urls import patterns
-from django.conf.urls import url
+# from django.conf.urls import patterns
+# from django.conf.urls import url
+
+# from networkapi.ambiente.resource.LogicalEnvironmentAddResource import LogicalEnvironmentAddResource
+# from networkapi.ambiente.resource.LogicalEnvironmentAlterRemoveResource import LogicalEnvironmentAlterRemoveResource
+# from networkapi.ambiente.resource.LogicalEnvironmentGetAllResource import LogicalEnvironmentGetAllResource
+
+# logical_environment_add_resource = LogicalEnvironmentAddResource()
+# logical_environment_alter_remove_resource = LogicalEnvironmentAlterRemoveResource()
+# logical_environment_get_all_resource = LogicalEnvironmentGetAllResource()
+
+# urlpatterns = patterns(
+#     '',
+#     url(r'^$', logical_environment_add_resource.handle_request,
+#         name='logical_environment.add'),
+#     url(r'^all/$', logical_environment_get_all_resource.handle_request,
+#         name='logical_environment.get.all'),
+#     url(r'^(?P<id_logicalenvironment>[^/]+)/$', logical_environment_alter_remove_resource.handle_request,
+#         name='logical_environment.update.remove.by.pk')
+# )
+
+#new
+
+# -*- coding: utf-8 -*-
+from django.urls import re_path
 
 from networkapi.ambiente.resource.LogicalEnvironmentAddResource import LogicalEnvironmentAddResource
 from networkapi.ambiente.resource.LogicalEnvironmentAlterRemoveResource import LogicalEnvironmentAlterRemoveResource
@@ -12,12 +35,11 @@ logical_environment_add_resource = LogicalEnvironmentAddResource()
 logical_environment_alter_remove_resource = LogicalEnvironmentAlterRemoveResource()
 logical_environment_get_all_resource = LogicalEnvironmentGetAllResource()
 
-urlpatterns = patterns(
-    '',
-    url(r'^$', logical_environment_add_resource.handle_request,
+urlpatterns = [
+    re_path(r'^$', logical_environment_add_resource.handle_request,
         name='logical_environment.add'),
-    url(r'^all/$', logical_environment_get_all_resource.handle_request,
+    re_path(r'^all/$', logical_environment_get_all_resource.handle_request,
         name='logical_environment.get.all'),
-    url(r'^(?P<id_logicalenvironment>[^/]+)/$', logical_environment_alter_remove_resource.handle_request,
+    re_path(r'^(?P<id_logicalenvironment>[^/]+)/$', logical_environment_alter_remove_resource.handle_request,
         name='logical_environment.update.remove.by.pk')
-)
+]
